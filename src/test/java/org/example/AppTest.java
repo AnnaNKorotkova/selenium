511bbf755c640de95b22aa43952ad798df50cf33
ruaabcn <anna.korotkova@raiffeisen.ru>
2022-10-24 15:59:52 +0300

    check sticker

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.BomInput.BytesProcessedNotification;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -47,12 +49,20 @@ public class AppTest extends BaseTest {
     List<WebElement> listElement = driver.findElements(By.cssSelector("ul[id=box-apps-menu] li"));
     int numberOfListElements = listElement.size();
     int k = 0;
-    for (int i = 0; i < numberOfListElements - 1; i++) {
+    for (int i = 0; i < numberOfListElements; i++) {
       List<WebElement> menu = driver.findElements(By.cssSelector("ul[id=box-apps-menu] li"));
       menu.get(i).click();
-      if (isElementPresent(driver, By.cssSelector("td[id=content] h1"))) {
-        k++;
+      if (isElementPresent(driver, By.cssSelector("ul[class=docs] li"))) {
+        List<WebElement> secondmenutList = driver.findElements(By.cssSelector("ul[class=docs] li"));
+        for (int j = 0; j < secondmenutList.size(); j++) {
+          List<WebElement> subMenu = driver.findElements(By.cssSelector("ul[class=docs] li"));
+          subMenu.get(j).click();
+           if (isElementPresent(driver, By.cssSelector("td[id=content] h1"))) {
+          k++;
+        }
+        }
       }
+        driver.navigate().to("http://localhost:8055/litecart/admin/");
     }
     System.out.println(k);
   }
@@ -62,23 +72,18 @@ public class AppTest extends BaseTest {
   public void checkSticker() {
     driver.navigate().to("http://localhost:8055/litecart/en/rubber-ducks-c-1/");
     int k = 0;
-    List<WebElement> listElement = driver.findElements(
-        By.cssSelector("div[class=content] ul.listing-wrapper.products li div.image-wrapper"));
+    List<WebElement> listElement = driver.findElements(By.cssSelector("div[class=content] ul.listing-wrapper.products a.link"));
     for (int i = 0; i < listElement.size(); i++) {
-      List<WebElement> newSticker = listElement.get(i)
-          .findElements(By.cssSelector("div.sticker.new"));
-      List<WebElement> saleSticker = listElement.get(i)
-          .findElements(By.cssSelector("div.sticker.sale"));
-      if(newSticker.size() == 1 || saleSticker.size() == 1){
+      if(isElementPresent(driver,By.cssSelector("div[class|=sticker]"))){
         k++;
       }
     }
-      Assertions.assertEquals(k,listElement.size());
+    Assertions.assertEquals(k, listElement.size());
   }
 
   boolean isElementPresent(WebDriver driver, By locator) {
     try {
-      driver.findElement(locator);
+      driver.findElements(locator);
       return true;
     } catch (NoSuchElementException ex) {
       return false;
