2484afa94ed7209e96439011aa4581239ec865d7
ruaabcn <anna.korotkova@raiffeisen.ru>
2022-11-08 10:03:33 +0300

    new product

--- src/test/java/org/example/BucketTest.java
+++ src/test/java/org/example/BucketTest.java
@@ -24,10 +24,14 @@ public class BucketTest extends BaseTest {
         for (int i = 0; i < elementList.size(); i++) {
             elementList.get(i).click();
         }
+        int afterDelete = Integer.parseInt(driver.findElement(
+                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
+
         driver.findElement(
                 By.xpath("//p/a")).click();
         int beforeDelete = Integer.parseInt(driver.findElement(
                 By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
+        System.out.println(afterDelete);
         System.out.println(beforeDelete);
     }
 
