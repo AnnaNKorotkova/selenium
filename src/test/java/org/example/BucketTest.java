092de20e8c0ea888b8c976d3cfb63485f0a0f4dd
ruaabcn <anna.korotkova@raiffeisen.ru>
2022-11-08 10:01:46 +0300

    new product

--- src/test/java/org/example/BucketTest.java
+++ src/test/java/org/example/BucketTest.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -15,43 +14,9 @@ public class BucketTest extends BaseTest {
 
     @Test
     public void bucketTest() {
-        driver.navigate().to("http://localhost:8055/litecart/en/");
-        List<WebElement> elements = driver.findElements(
-                By.xpath("//div[@class='content']/ul/li/a[@title='Green Duck'][1]"));
-        elements.get(0).click();
-
-        driver.findElement(
-                By.xpath("//button[@name='add_cart_product']")).click();
-
-        int count = Integer.parseInt(driver.findElement(
-                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
-
-        isElementNotPresent(driver, count);
-        count = Integer.parseInt(driver.findElement(
-                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
-
-        driver.navigate().to("http://localhost:8055/litecart/en/");
-        List<WebElement> elementsSecond = driver.findElements(
-                By.xpath("//div[@class='content']/ul/li/a[@title='Green Duck'][1]"));
-        elementsSecond.get(0).click();
-        driver.findElement(
-                By.xpath("//button[@name='add_cart_product']")).click();
-        isElementNotPresent(driver, count);
-
-        count = Integer.parseInt(driver.findElement(
-                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
-
-        driver.navigate().to("http://localhost:8055/litecart/en/");
-        List<WebElement> elementsThird = driver.findElements(
-                By.xpath("//div[@class='content']/ul/li/a[@title='Green Duck'][1]"));
-        elementsThird.get(0).click();
-        driver.findElement(
-                By.xpath("//button[@name='add_cart_product']")).click();
-        isElementNotPresent(driver, count);
-
-        count = Integer.parseInt(driver.findElement(
-                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
-
+        for (int i = 0; i < 3; i++) {
+            addElement();
+        }
         driver.findElement(
                 By.xpath("//div[@id='cart']/a[@class='link']")).click();
         List<WebElement> elementList = driver.findElements(
@@ -63,14 +28,16 @@ public class BucketTest extends BaseTest {
                 By.xpath("//p/a")).click();
         int beforeDelete = Integer.parseInt(driver.findElement(
                 By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText());
-        System.out.println(count);
         System.out.println(beforeDelete);
     }
 
-    void isElementNotPresent(WebDriver driver, int count) {
-        while (Integer.parseInt(driver.findElement(
-                By.xpath("//div[@id='cart']/a/span[@class='quantity']")).getText()) <= count) {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        }
+    void addElement() {
+        driver.navigate().to("http://localhost:8055/litecart/en/rubber-ducks-c-1/");
+        driver.findElements(By.xpath("//div[@class ='content']/ul[2]/li/a[@class='link']")).get(0)
+                .click();
+        driver.findElement(
+                By.xpath("//button[@name='add_cart_product']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
     }
 }
