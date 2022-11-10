8507268bf33cae1586b11d116a42f6eea7a8fc7d
ruaabcn <anna.korotkova@raiffeisen.ru>
2022-11-10 10:48:20 +0300

    logging

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -3,12 +3,14 @@ package org.example;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.logging.Level;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.decorators.Decorated;
 import org.openqa.selenium.support.decorators.WebDriverDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -31,6 +33,8 @@ public class BaseTest {
     }
     ChromeOptions options = new ChromeOptions();
     driver = new  ChromeDriver(options);
+    LoggingPreferences prefs = new LoggingPreferences();
+    prefs.enable("browser", Level.ALL);
     decorated = new LoggingDecorator().decorate(driver);
 
     tlDriver.set(driver);
